d2ecccd5e16b1e003b71003cb548b2fb1a74ee1d
Jonas Rembser <jonas.rembser@cern.ch>
2021-06-22 14:22:35 +0200

    [RF] Lazy import of RooFit in pythonization module
    This is important so we don't get the RooFit banner everytime pyROOT is
    initialized.

--- bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/_rooglobalfunc.py
+++ bindings/pyroot/pythonizations/python/ROOT/pythonization/_roofit/_rooglobalfunc.py
@@ -1,10 +1,24 @@
-from cppyy.gbl import RooFit
-
 from ._utils import _kwargs_to_roocmdargs
 
+RooFit = None
+
+def _RooFit():
+    """Does lazy import of RooFit namespace and then returns it.
+
+    We can't do `from cppyy.gbl import RooFit` directly, because then we would
+    see the RooFit banner every time pyROOT is initialized.
+
+    Code inspired by:
+    https://wiki.python.org/moin/PythonSpeed/PerformanceTips#Import_Statement_Overhead
+    """
+    global RooFit
+    if RooFit is None:
+        from cppyy.gbl import RooFit
+    return RooFit
+
 def LineColor(*args, **kwargs):
-    return RooFit._LineColor(*args, **kwargs)
+    return _RooFit()._LineColor(*args, **kwargs)
 
 def FitOptions(*args,**kwargs):
     args, kwargs = _kwargs_to_roocmdargs(*args, **kwargs)
-    return RooFit._FitOptions(*args, **kwargs)
+    return _RooFit()._FitOptions(*args, **kwargs)
