97c3ec8643ba5d6149af997cc5451772b16d28f3
Jonas Rembser <jonas.rembser@cern.ch>
2023-01-10 11:59:40 +0100

    [RF] Inline infinity checks in `RooNumber` and mark them `constexpr`
    This is mainly a performance optimization, because calling a small
    funciton that uses a static variable is quite expensive, given the
    simple infinity check.
    
    Also, it is not necessary to have a `ClassDef` macro for RooNumber,
    which is only a simple helper class to deal with infinities.
    
    Furthermore, the code hidden behind the `HAS_NUMERIC_LIMITS`
    preprocessor define is removed, because this was never used. The macro
    is never defined in any build configuration.

--- roofit/roofitcore/inc/RooNumber.h
+++ roofit/roofitcore/inc/RooNumber.h
@@ -1,35 +1,37 @@
-/*****************************************************************************
- * Project: RooFit                                                           *
- * Package: RooFitCore                                                       *
- *    File: $Id: RooNumber.h,v 1.7 2007/05/11 09:11:30 verkerke Exp $
- * Authors:                                                                  *
- *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
- *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
- *                                                                           *
- * Copyright (c) 2000-2005, Regents of the University of California          *
- *                          and Stanford University. All rights reserved.    *
- *                                                                           *
- * Redistribution and use in source and binary forms,                        *
- * with or without modification, are permitted according to the terms        *
- * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
- *****************************************************************************/
-#ifndef ROO_NUMBER
-#define ROO_NUMBER
+/*
+ * Project: RooFit
+ *
+ * Copyright (c) 2023, CERN
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted according to the terms
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
 
-#include "Rtypes.h"
+/**
+\file RooNumber.h
+\class RooNumber
+\ingroup Roofitcore
+
+RooNumber implements numeric constants used by RooFit.
+**/
+
+#ifndef RooFit_RooNumber_h
+#define RooFit_RooNumber_h
 
 class RooNumber {
 public:
+   /// Return internal infinity representation.
+   static constexpr double infinity() { return _inf; }
 
-  virtual ~RooNumber() {} ;
+   /// Return sign(x) if x is infinite by the RooFit-internal specification,
+   /// otherwise return zero.
+   static constexpr int isInfinite(double x) { return (x >= +_inf) ? +1 : ((x <= -_inf) ? -1 : 0); }
 
-  static double infinity() ;
-  static Int_t isInfinite(double x) ;
-
- private:
-  static double _Infinity ;
-
-  ClassDef(RooNumber,0) // wrapper class for portable numerics
+private:
+   // This assumes a well behaved IEEE-754 floating point implementation. The
+   // next line may generate a compiler warning that can be ignored.
+   static constexpr double _inf = 1.e30;
 };
 
 #endif
