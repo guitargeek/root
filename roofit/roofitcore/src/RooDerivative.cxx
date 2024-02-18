f9a98bc2333db1b3034b7a14b555582a2410edbc
langenbruch <57998197+langenbruch@users.noreply.github.com>
2024-02-18 13:43:09 +0100

    Update roofit/roofitcore/src/RooDerivative.cxx
    Co-authored-by: Jonas Rembser <jonas.rembser@cern.ch>

--- roofit/roofitcore/src/RooDerivative.cxx
+++ roofit/roofitcore/src/RooDerivative.cxx
@@ -105,10 +105,9 @@ double RooDerivative::evaluate() const
   if (!_ftor) {
     _ftor = std::unique_ptr<RooFunctor>{_func.arg().functor(_x.arg(),RooArgSet(),_nset)};
     ROOT::Math::WrappedFunction<RooFunctor&> wf(*_ftor);
-    if (_x.hasMin() && _x.hasMax())
-      _rd = std::make_unique<ROOT::Math::RichardsonDerivator>(wf,_eps*(_x.max()-_x.min()),true);
-    else
-      _rd = std::make_unique<ROOT::Math::RichardsonDerivator>(wf,_eps,true);      
+    // epsilon is relative for bounded parameters, and absolute for unbounded
+    const double eps = _x.hasMin() && _x.hasMax() ? _eps* (_x.max() - _x.min()) : _eps;
+    _rd = std::make_unique<ROOT::Math::RichardsonDerivator>(wf, eps, true);      
   }
 
   switch (_order) {
