3cdd511b24ca3f5932d795396f945b38aed88aec
Jonas Rembser <jonas.rembser@cern.ch>
2025-03-26 10:31:07 +0100

    Create myvecops

---
+++ math/myvecops/inc/LinkDef.h
@@ -0,0 +1,35 @@
+/*************************************************************************
+ * Copyright (C) 1995-2021, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * Author: Danilo Piparo - CERN                                          *
+ * For the licensing terms see $MyROOTSYS/LICENSE.                         *
+ * For the list of contributors see $MyROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifdef __CLING__
+
+#pragma link C++ namespace MyROOT::VecOps;
+
+#pragma link C++ class MyROOT::Internal::VecOps::SmallVectorBase-;
+
+#pragma link C++ class MyROOT::VecOps::RVec<bool>-;
+
+#pragma link C++ class MyROOT::VecOps::RVec<float>-;
+#pragma link C++ class MyROOT::VecOps::RVec<double>-;
+
+#pragma link C++ class MyROOT::VecOps::RVec<char>-;
+#pragma link C++ class MyROOT::VecOps::RVec<short>-;
+#pragma link C++ class MyROOT::VecOps::RVec<int>-;
+#pragma link C++ class MyROOT::VecOps::RVec<long>-;
+#pragma link C++ class MyROOT::VecOps::RVec<long long>-;
+
+#pragma link C++ class MyROOT::VecOps::RVec<unsigned char>-;
+#pragma link C++ class MyROOT::VecOps::RVec<unsigned short>-;
+#pragma link C++ class MyROOT::VecOps::RVec<unsigned int>-;
+#pragma link C++ class MyROOT::VecOps::RVec<unsigned long>-;
+#pragma link C++ class MyROOT::VecOps::RVec<unsigned long long>-;
+
+#pragma link C++ class MyROOT::VecOps::RVec<std::string>-;
+
+#endif
